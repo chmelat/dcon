e7c4b80e817dc04759a799dbd167315b6d780ccd
Tomas Chmela <chmela@ujp.cz>
2020-03-20 15:10:52 +0100

    minimal changes

--- dcon_start.sh
+++ dcon_start.sh
@@ -3,7 +3,7 @@
   while true
   do	  
     datum=`date +%y%m%d-%H`  # datum
-    ./dcon -a 84 -i 60 -w 2 | tee -a data_${datum}.dat >/dev/null
+    ./dcon -a 01 -i 60 -w 2 | tee -a data_${datum}.dat >/dev/null
     if [ -s data_${datum}.dat ]; then  # Soubor je nenulove delky
       tar cfz data_${datum}.tgz data_${datum}.dat && rm data_${datum}.dat
     fi
