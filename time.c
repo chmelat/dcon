1feac71289a55fbb82bb7a71bd5d7399f04845ef
Tomas <zero@ch>
2020-06-27 09:47:13 +0200

    Print output time in ms

--- time.c
+++ time.c
@@ -3,6 +3,7 @@
  *  V1.0/2012-12-05
  *  V1.1/2020-02-10 Add parameter int f
  *  V1.2/2020-06-21 New format of filename: aa_yymmddhhmmss.dat, f changed to adr
+ *  V1.3/2020-06-27 Output time in [ms]
  */
 #include <stdio.h>  /* Standard input/output definitions */
 #include <stdlib.h>  /* Standard lib */
@@ -38,7 +39,7 @@ char *now(unsigned char adr)
   if (adr==0) { /* Output time format */
     /* Conversion time structure to desired form */
     strftime(buf_tmp,sizeof(buf),"%Y-%m-%d %H:%M:%S",ts);
-    snprintf(buf,DBUF,"%s.%02u",buf_tmp,(unsigned)(now.tv_usec/1E4));  /* Add fraction */
+    snprintf(buf,DBUF,"%s.%03ud",buf_tmp,(unsigned int)(now.tv_usec/1E3));  /* Add fraction (ms) */
    /* printf("%06u\n", now.tv_usec); */  /*ladici*/
   }
   else  {  /* Name of file in format: aa_yymmddhhmmss.dat */
